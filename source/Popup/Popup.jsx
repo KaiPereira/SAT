2f94242ff0a37f16fdf119faf0938dd9eb01d55d
KaiPereira <88850028+KaiPereira@users.noreply.github.com>
2022-11-14 17:10:25 -0800

    Made it better :)

--- source/Popup/Popup.jsx
+++ source/Popup/Popup.jsx
@@ -3,25 +3,34 @@ import browser from 'webextension-polyfill';
 import './styles.scss';
 import axios from "axios"
 
-function openWebPage(url) {
-  return browser.tabs.create({url});
-}
-
 const Popup = () => {
+  const token = "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"
+
+
+
+
+
+  
+
+
+
+
+  // All of the states
   const [search, changeSearch] = React.useState()
   const [answer, changeAnswer] = React.useState()
   const [pageDetails, changePageDetails] = React.useState()
 
+  // Input search function because it's React :(
   function changeSearchFunction(e) {
     changeSearch(e.target.value)
-    console.log(search)
   }
 
+  // Function to request the model from hugging face
   async function query(data) {
     const response = await fetch(
       "https://api-inference.huggingface.co/models/deepset/roberta-base-squad2",
       {
-        headers: { Authorization: "Bearer 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" },
+        headers: { Authorization: `Bearer ${token}` },
         method: "POST",
         body: JSON.stringify(data),
       }
@@ -30,10 +39,7 @@ const Popup = () => {
     return result;
   }
 
-  // chrome.runtime.sendMessage("Hello", (response) => {
-  //   localStorage.setItem("pageDetails", response)
-  // });
-
+  // Function to get the answer to a question using the query function
   function getAnswer() {
       query({"inputs": {
         "question": search,
@@ -43,6 +49,7 @@ const Popup = () => {
     });
   } 
 
+  // Fetches all the page text to use for the context in the model
   function modifyDOM() {
     console.log('Tab script:');
     console.log(document.body);
@@ -56,6 +63,7 @@ const Popup = () => {
       changePageDetails(results[0]);
   });
 
+  // Frontend
   return (
     <main id="popup">
       <input type="text" className="searchBar" onChange={changeSearchFunction} />
